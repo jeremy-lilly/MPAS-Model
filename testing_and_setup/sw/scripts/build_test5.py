c8d1364b3eada8f96267826c7995caf356358cdc
Jeremy Lilly <lillyj@oregonstate.edu>
2021-06-29 16:07:55 -0600

    Add WIP unified script to build and config test cases

---
+++ testing_and_setup/sw/scripts/build_test5.py
@@ -0,0 +1,192 @@
+#!/usr/bin/env python
+
+import argparse as ap
+import os
+import subprocess as sp
+import xml.etree.ElementTree as et
+from build_base_mesh_test5 import main as build_base_mesh
+from build_weighted_graph_info_with_lts_regions_test5 import main as build_graph
+from build_graph_info_part_for_multi_block_run import main as partition_graph
+
+
+def main(outDir, modelRepo, baseMesh, graphInfo, model, numProcs, multiBlocks,
+         numExtraInterface, coarseRegionDist, doLTS2):
+    
+    startingDir = os.getcwd()
+    
+    if outDir[-1] != '/':
+        outDir += '/'
+
+    if modelRepo[-1] != '/':
+        modelRepo += '/'
+
+    # -- BEGIN --
+    # mesh generation and MPI partitioning
+    
+    #os.chdir(outDir)
+
+    #print('\n\n\n--- Building base mesh...\n\n\n')
+    #build_base_mesh(baseMesh, True)
+    #print('\n\n\n--- Done\n\n\n')
+
+    #print('\n\n\n--- Converting ' + baseMesh + ' to a MPAS mesh...\n\n\n')
+    #shCommand = 'MpasMeshConverter.x ' + baseMesh
+    #sp.call(shCommand.split())
+
+    #shCommand = 'mv graph.info ' + graphInfo
+    #sp.call(shCommand.split())
+    #print('\n\n\n--- Done\n\n\n')
+
+    #print('\n\n\n--- Weighting ' + graphInfo + ' for LTS regions...\n\n\n')
+    #build_graph(baseMesh, graphInfo, coarseRegionDist, doLTS2)
+    #print('\n\n\n--- Done\n\n\n')
+
+    #print('\n\n\n--- Partitioning cells across MPI blocks with gpmetis...\n\n\n')
+    #if multiBlocks:
+    #    numBlocks = 3 * numProcs
+    #else:
+    #    numBlocks = numProcs
+
+    #shCommand = 'gpmetis ' + graphInfo + ' ' + str(numBlocks)
+    #sp.call(shCommand.split())
+    #print('\n\n\n--- Done\n\n\n')
+
+    #print('\n\n\n--- Resorting cells so that each MPI block only has one \n \
+    #      type of cell--fine, interface, or coarse...\n\n\n')
+    #partition_graph(baseMesh, graphInfo, numBlocks)
+    #print('\n\n\n--- Done\n\n\n')
+
+    # -- END --
+
+
+    # -- BEGIN --
+    # model config and build
+
+    os.chdir(modelRepo)
+
+    if doLTS2:
+        nLTSHalos = 1
+        LTSX = 'LTS2'
+    else:
+        nLTSHalos = 3
+        LTSX = 'LTS3'
+
+    if numExtraInterface != 0:
+        nLTSHalosCopy = (nLTSHalos - 1) + numExtraInterface
+        moreCellsOnInterface = 1
+    else:
+        nLTSHalosCopy = nLTSHalos
+        moreCellsOnInterface = 0
+
+
+    print('\n\n\n--- Editing Registry.xml...\n\n\n')
+    registryXML = modelRepo + 'src/core_sw/Registry.xml'
+
+    registryTree = et.parse(registryXML)
+    registryRoot = registryTree.getroot()
+
+    registryDims = registryRoot.find('dims').findall('dim')
+    for dim in registryDims:
+        name = dim.attrib['name']
+        if name == 'nLTSHalos':
+            dim.set('definition', str(nLTSHalos))
+        elif name == 'moreCellsOnInterface':
+            dim.set('definition', str(moreCellsOnInterface))
+        elif name == 'nLTSHalosPExtra':
+            dim.set('definition', str(nLTSHalosCopy))
+
+    registryTree.write(registryXML) 
+    print('\n\n\n--- Done\n\n\n')
+
+    print('\n\n\n--- Building model...')
+    shCommand = 'make clean CORE=sw'
+    sp.call(shCommand.split())
+
+    shCommand = 'make gfortran CORE=sw DEBUG=true'
+    sp.call(shCommand.split())
+
+    shCommand = 'cp sw_model ' + outDir + model
+    sp.call(shCommand.split())
+    print('\n\n\n--- Done\n\n\n')
+
+    print('\n\n\n--- Configuring namelist.sw and streams.sw...\n\n\n')
+    shCommand = 'cp namelist.sw streams.sw ' + outDir
+    sp.call(shCommand.split())
+
+    os.chdir(outDir)
+
+    # change fields of namelist and streams here
+    print('\n\n\n--- Done\n\n\n')
+
+    # -- END --
+
+
+# END main
+
+
+if __name__ == '__main__':
+    # If called as the primary module, run main
+
+    parser = ap.ArgumentParser(description='Python script to generate the base \
+                               mesh, graph.info file, block partition, and \
+                               executable for running a variation of test case \
+                               5 from Williamson et al.')
+
+    parser.add_argument('-o', '--out-dir', dest='out_dir', required=True,
+                        type=str,
+                        help='Directory to store all output files in.')
+
+    parser.add_argument('-r', '--model-repo', dest='model_repo', required=True,
+                        type=str,
+                        help='Path to the `MPAS-Model` repo to build the \
+                        model from.')
+
+    parser.add_argument('-b', '--base-mesh', dest='base_mesh',
+                        default='base_mesh.nc', type=str,
+                        help='Filename for the base mesh. Default is \
+                        `base_mesh.nc`.')
+
+    parser.add_argument('-g', '--graph-info', dest='graph_info',
+                        default='graph.info', type=str,
+                        help='Filename for graph.info file. Default is \
+                        `graph.info`.')
+
+    parser.add_argument('-m', '--model', default='sw_model', type=str,
+                        help='Filename for model executable. Default is \
+                        `sw_model`.')
+
+    parser.add_argument('-n', '--num-procs', dest='num_procs',
+                        default=128, type=int,
+                        help='Number of MPI processors to config for. Default \
+                        is 128.')
+
+    parser.add_argument('-k', '--multi-blocks-per-proc', dest='multi_blocks',
+                        action='store_true',
+                        help='Set to partition the graph so that each MPI \
+                        processor has three blocks--one for each type of LTS \
+                        region i.e. NUM_BLOCKS = 3 * NUM_PROCS.')
+
+    parser.add_argument('-e', '--num-extra-interface-layers', 
+                        dest='num_extra_interface', default=0, type=int,
+                        help='Number of extra interface layers to add for load \
+                        balancing. Default is 0. NOTE: LTS3 requires that 2 is \
+                        added to this number--THIS IS HANDELED AUTOMATICALLY. \
+                        For example, running LTS3 and setting this flag to 38 \
+                        results in 40 total interface layers for each region.')
+
+    parser.add_argument('-c', '--coarse-region-dist', 
+                        dest='coarse_region_dist', default=0.55, type=float,
+                        help='Cells more than this distance way from the \
+                        mountain at the north pole will be part of the coarse \
+                        region. Default is 0.55.')
+
+    parser.add_argument('--lts2', dest='do_lts2', action='store_true',
+                        help='Prepare the mesh for LTS2 rather than LTS3 which \
+                        is the default.')
+
+    args = parser.parse_args()
+
+    main(args.out_dir, args.model_repo, args.base_mesh, args.graph_info, 
+         args.model, args.num_procs, args.multi_blocks, 
+         args.num_extra_interface, args.coarse_region_dist, args.do_lts2)
+
