d12284030a5bcddf11f03bcfc7d192a1086a507e
Jeremy Lilly <lillyj@oregonstate.edu>
2022-01-27 11:52:37 -0700

    Add initial versions of lts setup scripts

---
+++ testing_and_setup/lts/label_mesh_lts.py
@@ -0,0 +1,266 @@
+#!/usr/bin/env python
+
+import os
+import subprocess as sp
+import shutil
+import numpy as np
+import xarray as xr
+import argparse as ap
+import math
+import time
+
+
+def main(base_mesh, graph_info, num_interface, coarse_region_dist, lts2):
+    timeStart = time.time()
+
+    ds = xr.open_dataset(base_mesh)
+
+    # Set to 1 for LTS2, 3 for LTS3
+    if lts2:
+        nLTSHalos = 1
+    else:
+        nLTSHalos = 3
+
+    # THE NUMBER SET HERE IS THE NUMBER OF EXTRA LTS HALO LAYERS 
+    # AND HAS TO BE THE SAME SET IN init_LTS 
+    # For example, if running LTS3 and num_interface == 38,
+    # Then set nLTSHalosCopy = 40 in init_LTS
+    # Also, if num_interface != 1, need to set
+    # moreCellsOnInterface = 1 on init_LTS
+    # We may want to change variable names here and in init LTS for readability
+    nLTSHalosCopy = nLTSHalos
+    if num_interface != 1:
+        nLTSHalosCopy = (nLTSHalos - 1) + num_interface
+
+    nCells = ds['nCells'].size
+    nEdges = ds['nEdges'].size
+    areaCell = ds['areaCell'].values
+    nEdgesOnCell = ds['nEdgesOnCell'].values
+    cellsOnEdge = ds['cellsOnEdge'].values
+    edgesOnCell = ds['edgesOnCell'].values
+    latCell = ds['latCell']
+    lonCell = ds['lonCell']
+    latPoint = math.pi / 6.0
+    lonPoint = 3.0 * math.pi * 0.5
+
+    LTSRegion = [1] * nCells
+    LTSRegionLocal = [1] * nCells
+
+    for iCell in range(0, nCells):
+        arg1 = np.sqrt(np.sin(0.5 * (latPoint - latCell[iCell])) ** 2 
+                + np.cos(latCell[iCell]) * np.cos(latPoint) 
+                * np.sin(0.5 * (lonPoint - lonCell[iCell])) ** 2)
+        sphereDistance = 2 * np.arcsin(arg1)
+
+        #CAREFUL: THIS HAS TO MATCH WHAT'S ON `src/core_sw/mpas_sw_test_cases.F` 
+        if sphereDistance > coarse_region_dist: 
+            LTSRegion[iCell] = 2
+            LTSRegionLocal[iCell] = 2
+
+    minMaxLTSRegion = [1] * 2
+    minMaxLTSRegion[0] = min(LTSRegionLocal)
+    minMaxLTSRegion[1] = max(LTSRegionLocal)
+
+    for iEdge in range(0, nEdges):
+        cell1 = cellsOnEdge[iEdge, 0] - 1 
+        cell2 = cellsOnEdge[iEdge, 1] - 1
+        
+        if (cell1 != -1 and cell2 != -1):
+
+            if ( (LTSRegionLocal[cell1] == minMaxLTSRegion[0]) 
+                    and (LTSRegionLocal[cell2] == minMaxLTSRegion[1]) ): 
+                LTSRegionLocal[cell1] = minMaxLTSRegion[0] + 2
+                LTSRegionLocal[cell2] = minMaxLTSRegion[1] + 2
+
+            elif ( (LTSRegionLocal[cell2] == minMaxLTSRegion[0]) 
+                    and (LTSRegionLocal[cell1] == minMaxLTSRegion[1]) ):
+                LTSRegionLocal[cell2] = minMaxLTSRegion[0] + 2
+                LTSRegionLocal[cell1] = minMaxLTSRegion[1] + 2
+
+            elif ( (LTSRegionLocal[cell1] == minMaxLTSRegion[0] + 2) 
+                    and (LTSRegionLocal[cell2] == minMaxLTSRegion[1]) ):
+                LTSRegionLocal[cell2] = minMaxLTSRegion[1] + 2
+
+            elif ( (LTSRegionLocal[cell2] == minMaxLTSRegion[0] + 2) 
+                    and (LTSRegionLocal[cell1] == minMaxLTSRegion[1]) ):
+                LTSRegionLocal[cell1] = minMaxLTSRegion[1] + 2
+
+            elif ( (LTSRegionLocal[cell1] == minMaxLTSRegion[0]) 
+                    and (LTSRegionLocal[cell2] == minMaxLTSRegion[1] + 2) ):
+                LTSRegionLocal[cell1] = minMaxLTSRegion[0] + 2
+
+            elif ( (LTSRegionLocal[cell2] == minMaxLTSRegion[0]) 
+                    and (LTSRegionLocal[cell1] == minMaxLTSRegion[1] + 2) ):
+                LTSRegionLocal[cell2] = minMaxLTSRegion[0] + 2
+
+    for iRegion in range(0, 2):
+        for iHalo in range(0, nLTSHalosCopy-1):
+            for iCell in range(0, nCells):
+                if (LTSRegionLocal[iCell] == ( minMaxLTSRegion[iRegion] 
+                    + 2 * (iHalo+1)) ):
+                    for i in range(0, nEdgesOnCell[iCell]):
+                        iEdge = edgesOnCell[iCell, i] - 1
+                        cell1 = cellsOnEdge[iEdge, 0] - 1
+                        cell2 = cellsOnEdge[iEdge, 1] - 1
+
+                        if (LTSRegionLocal[cell1] == minMaxLTSRegion[iRegion]):
+                            LTSRegionLocal[cell1] = ( LTSRegionLocal[cell1] 
+                                +  2 * (iHalo + 2) )
+                        elif (LTSRegionLocal[cell2] == minMaxLTSRegion[iRegion]):
+                            LTSRegionLocal[cell2] = ( LTSRegionLocal[cell2] 
+                                +  2 * (iHalo + 2) )
+
+    if num_interface != 1:
+        # this means we are using LTS2
+        if (nLTSHalos == 1): 
+            for iCell in range(0, nCells):
+                
+                # if it's odd and not 1 (fine) then must
+                # be 3 (interface layer 1)
+                if ( (LTSRegionLocal[iCell] % 2 == 1) 
+                        and (LTSRegionLocal[iCell] != 1) ):
+                    LTSRegionLocal[iCell] = 3
+                
+                # if it's even and not 2 (coarse) then 
+                # must be 4 (interface layer 2)
+                elif ( (LTSRegionLocal[iCell] % 2 == 0) 
+                        and (LTSRegionLocal[iCell] != 2) ): 
+                    LTSRegionLocal[iCell] = 4
+        
+        # this means we are using LTS3
+        elif (nLTSHalos == 3): 
+            for iCell in range(0, nCells) :
+                if( (LTSRegionLocal[iCell] % 2 == 1) 
+                        and (LTSRegionLocal[iCell] != 1) ):
+                    # if we are here it could be either 
+                    # interface 1 or those two layers of 
+                    # fine we need for the third order
+                    if ( LTSRegionLocal[iCell] 
+                            == 2 * nLTSHalosCopy + 1 ):
+                        LTSRegionLocal[iCell] = 7
+                    elif ( LTSRegionLocal[iCell] 
+                            == 2 * nLTSHalosCopy - 1 ):
+                        LTSRegionLocal[iCell] = 5 
+                    else:
+                        LTSRegionLocal[iCell] = 3 
+                
+                elif ( (LTSRegionLocal[iCell] % 2 == 0) 
+                        and (LTSRegionLocal[iCell] != 2) ):
+                    # if we are here it could be either interface 2 
+                    # or those two layers of coarse
+                    if (LTSRegionLocal[iCell] == 2 * nLTSHalosCopy + 2):
+                        LTSRegionLocal[iCell] = 8 
+                    elif (LTSRegionLocal[iCell] == 2 * nLTSHalosCopy):
+                        LTSRegionLocal[iCell] = 6 
+                    else :
+                        LTSRegionLocal[iCell] = 4 
+
+    
+    # create mesh_with_lts_regions.nc 
+    LTSDataArray = xr.DataArray(LTSRegionLocal)
+    LTSDict = {'LTSRegionLocal': (['nCells'], LTSDataArray.data)}
+    LTSds = xr.Dataset(LTSDict)
+
+    encodingDict = {'LTSRegionLocal': {'_FillValue': -1}}
+    
+    combined = xr.merge([ds, LTSds], combine_attrs='drop_conflicts')
+    combined.to_netcdf(path='mesh_with_lts.nc', 
+                       encoding=encodingDict,
+                       format='NETCDF3_64BIT')
+
+    shCommand = 'paraview_vtk_field_extractor.py -f mesh_with_lts.nc \
+                 -v allOnCells,allOnEdges -d TWO= maxEdges=  maxEdges2= \
+                 -o mesh_with_lts_vtk'
+    sp.call(shCommand.split())
+
+    
+    # label cells in graph.info
+    fineCells = 0
+    coarseCells = 0
+    
+    newf = ""
+    with open(graph_info,'r') as f:
+        lines = f.readlines()
+        newf += lines[0].strip() + " 010 3 \n"
+        for iCell in range(1,len(lines)):
+            if ( LTSRegionLocal[iCell-1] == 1 
+                    or LTSRegionLocal[iCell-1] == 5 
+                    or LTSRegionLocal[iCell-1] == 7 ):  # fine 
+                newf+= "0 1 0 " + lines[iCell].strip() + "\n"
+                fineCells = fineCells + 1
+            elif ( LTSRegionLocal[iCell-1] == 2 
+                    or LTSRegionLocal[iCell-1] == 6 
+                    or LTSRegionLocal[iCell-1] == 8 ):  # coarse
+                newf+= "1 0 0 " + lines[iCell].strip() + "\n"
+                coarseCells = coarseCells + 1
+            else:  # interface 1 and 2
+                newf+= "0 0 1 " + lines[iCell].strip() + "\n"
+                coarseCells = coarseCells + 1 
+        
+    with open(graph_info, 'w') as f:
+        f.write(newf)
+
+    areaRatio = max(areaCell) / min(areaCell)
+    numberRatio = coarseCells / fineCells
+
+    print('Number of fine cells = ' + str(fineCells))
+    print('Number of coarse cells = ' + str(coarseCells))
+    print('Ratio of largest cell area to smallest cell area = ' 
+          + str(areaRatio))
+    print('Ratio of number of coarse cells to number of fine cells = '
+          + str(numberRatio))
+
+    return fineCells, coarseCells, areaRatio, numberRatio
+
+
+# END of main()
+
+
+if __name__ == '__main__':
+    # If called as a primary module, run main
+
+    parser = ap.ArgumentParser(description='Python script to label the cells \
+                               from a base mesh for test case 5 from \
+                               Williamson et al. for LTS. This script \
+                               MODIFIES the graph.info file specified in its \
+                               command-line arguments. Note that \
+                               before running this script on a base \
+                               mesh, one should convert it to a valid \
+                               MPAS mesh with `MpasMeshConverter.x \
+                               <base_mesh>` -- doing this also produces \
+                               a corresponding graph.info file.')
+
+    parser.add_argument('-b', '--base-mesh', dest='base_mesh',
+                        default='base_mesh.nc',
+                        help='File containing the base mesh. Default is \
+                        `base_mesh.nc`.')
+
+    parser.add_argument('-g', '--graph-info', dest='graph_info',
+                        default='graph.info',
+                        help='The graph.info file corresponding to the base \
+                        mesh. Default is `graph.info`.')
+
+    parser.add_argument('-l', '--num-interface-layers', 
+                        dest='num_interface', default=1, type=int,
+                        help='Number of extra interface layers to add for load \
+                        balancing. Default is 1. NOTE: LTS3 requires that 2 is \
+                        added to this number--THIS IS HANDELED AUTOMATICALLY. \
+                        For example, running LTS3 and setting this flag to 38 \
+                        results in 40 total interface layers for each region.')
+
+    parser.add_argument('-c', '--coarse-region-dist', 
+                        dest='coarse_region_dist', default=0.55, type=float,
+                        help='Cells more than this distance way from the \
+                        mountain at the north pole will be part of the coarse \
+                        region. Default is 0.55.')
+
+    parser.add_argument('--lts2', action='store_true',
+                        help='Prepare the mesh for LTS2 rather than LTS3 which \
+                        is the default.')
+
+    args = parser.parse_args()
+
+
+    main(args.base_mesh, args.graph_info, args.num_interface,
+         args.coarse_region_dist, args.lts2)
+
---
+++ testing_and_setup/lts/repartition_graph_info_part_lts.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+
+import os
+import shutil
+import numpy as np
+import xarray as xr
+import argparse
+import math
+import time
+
+
+def main(base_mesh, graph_info, num_blocks):
+    timeStart = time.time()
+
+    ds = xr.open_dataset(base_mesh)
+
+    nCells = ds['nCells'].size
+    LTSRegionLocal = np.zeros([nCells])
+
+    with open(graph_info, 'r') as gi:
+        lines = gi.readlines()
+        for iCell in range(1, len(lines)):
+            if lines[iCell][0:5] == '0 1 0':
+                LTSRegionLocal[iCell-1] = int(1)  # fine
+            elif lines[iCell][0:5] == '1 0 0':
+                LTSRegionLocal[iCell-1] = int(2)  # coarse
+            else:
+                LTSRegionLocal[iCell-1] = int(3)  # interface
+
+    numBlocks = num_blocks  # usually 3 * NUM_PROCS
+
+    newf=""
+    with open(graph_info + '.part.' + str(int(numBlocks)), 'r') as f:
+        lines = f.readlines()
+        procFoundCell = [0] * nCells
+
+        for iCell in range(0, len(lines)):
+            procCell = int(numBlocks / 3 + 1)  # just some initialization
+            for iProc in range(0, int(numBlocks/3)) :
+                # NOTE: the if statement below is like that because the 
+                # blocks assigned to a given processor go like 
+                # iProc*2 + iProc + k, where k=0,1,2
+                for k in range(0, 3) :
+                    if int(lines[iCell][0:10]) == (iProc*2 + iProc + k) :
+                        procCell = int(iProc)
+                        procFoundCell[iCell] = 1
+                        break
+                if procFoundCell[iCell] == 1 :
+                    break
+
+            blockID = str(numBlocks + 1)  # just some initialization
+            if LTSRegionLocal[iCell] == 1:
+                # blocks 0,3,6,9 etc are FINE
+                tmp = procCell * 2 + procCell
+                blockID = str(tmp)
+            elif LTSRegionLocal[iCell] == 2:   
+                # blocks 1,4,7,10 etc are COARSE
+                tmp = procCell * 2 + procCell + 1
+                blockID = str(tmp)
+            else:
+                # blocks 2,5,8,11 etc are INTERFACE
+                tmp = procCell * 2 + procCell + 2
+                #tmp = 2 #this gives all the interface cells to block 2 
+                # (case C in the paper)
+                blockID = str(tmp)
+                #if procCell % 2 == 0 :
+                #   tmp = 2
+                #   blockID = str(tmp)
+                #else :
+                #   tmp = 5
+                #   blockID = str(tmp)
+            
+            newf+= blockID + "\n"
+       
+        print('if all procCells have been found, these two numbers are equal:',
+                sum(procFoundCell[:]), nCells)
+    
+    with open(graph_info + '.part.' + str(int(numBlocks)), 'w') as f:
+        f.write(newf)
+
+
+if __name__ == '__main__':
+    # If called as a primary module, run main
+
+    parser = argparse.ArgumentParser(description='Python script to repartition \
+                                     an existing graph.info.part. file so that \
+                                     each block contains only one type of cell \
+                                     from fine, coarse, and interface. Before \
+                                     running this script, one must generate a \
+                                     graph.info.part.NUM_BLOCKS file from an \
+                                     existing graph.info file by running \
+                                     `gpmetis graph.info NUM_BLOCKS`.')
+
+    parser.add_argument('-b', '--base-mesh', dest='base_mesh',
+                        default='base_mesh.nc',
+                        help='File containing the base mesh. Default is \
+                        `base_mesh.nc`.')
+
+    parser.add_argument('-g', '--graph-info', dest='graph_info',
+                        default='graph.info',
+                        help='graph.info file corresponding to the base mesh. \
+                        Default is `graph.info`. The script assumes that the \
+                        corresponding .part.NUM_BLOCKS file is in the same \
+                        directory as this file.')
+
+    parser.add_argument('-k', '--num-blocks', dest='num_blocks',
+                        default=12,
+                        help='Number of blocks to partition for. Usually this \
+                        is 3 * NUM_PROCS, where NUM_PROCS is the number of MPI \
+                        processors that will be used. Default is 12.')
+
+    args = parser.parse_args()
+
+
+    main(args.base_mesh, args.graph_info, args.num_blocks)
+
